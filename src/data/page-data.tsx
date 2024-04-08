521f41835dca5fcf6a14847099051475e884502f
junicoder <muhammadjuni424@gmail.com>
2024-04-09 01:56:34 +0500

    first commit

---
+++ src/data/page-data.tsx
@@ -0,0 +1,66 @@
+export const personalData = {
+	name: "Junaid",
+	role: " Web Developer",
+	education: [
+		"Bachelor of Science in Computer Science, University of UMT(2023 - Continue)",
+		"Intermediate ICS from Concordia college garden town lahore(2021-2022)",
+	],
+	contactLinks: [
+		"mailto:emailto:junimahardev@gmail.com",
+		"https://twitter.com/time_tocode",
+		"https://linkedin.com/aliciarodriguezLi",
+		"https://medium.com/@time2code.ali",
+	],
+};
+
+export const aboutMe = {
+	title: "About Me",
+	body: [
+		"As a BS CS student at UMT, my passion for coding and design drives me forward every day. I thrive on exploring new technologies and pushing the boundaries of what's possible. Currently, I'm immersing myself in the world of Web 3.0 and AI through the PIAIC course, eager to uncover its potential and contribute to its evolution. Being a tech enthusiast isn't just a hobby for me—it's a way of life. With every line of code I write and every design I create, I'm shaping the future of technology. I'm excited to see where this journey takes me and the impact I can make in the tech world.",
+		"When I'm not working, I love designing, watching memes, and spending time with my friends and family. I believe that my determination, can-do attitude, and hard work make me a true asset to any team.",
+	],
+};
+
+export const skills = {
+	soft: [
+		{ icon: "👂🏼", text: "Active Listening" },
+		{ icon: "💬", text: "Effective Communication" },
+		{ icon: "👥", text: "Collaboration" },
+		{ icon: "⽓", text: "Teamwork" },
+		{ icon: "💡", text: "Creative Problem Solving" },
+		{ icon: "⌛️", text: "Time management" },
+	],
+	hard: [
+		{ icon: "💻", text: "Python" },
+		{ icon: "ʦ", text: "TypeScript" },
+		{ icon: "🚀", text: "React" },
+		{ icon: "💾", text: "SQL" },
+		{ icon: "💾", text: "noSQL" },
+		{ icon: "📈", text: "Data Structures and Algorithms" },
+	],
+};
+
+export const professionalData = {
+	title: "Professional Experience",
+	experiences: [
+		{
+			role: "Data Scientist, Acme Inc. (2021-Present)",
+			description:
+				"Design and implement machine learning models to analyze large datasets and drive business decisions. Collaborate with cross-functional teams to develop and launch new products and features. Provide technical guidance and mentorship to junior data scientists. Conduct regular presentations to stakeholders on the findings and insights generated from data analysis.",
+			current: true,
+		},
+
+		{
+			role: "Machine Learning Engineer, XYZ Corp. (2019-2021)",
+			description:
+				"Designed and implemented advanced machine learning algorithms to improve predictive accuracy and performance. Built and maintained scalable machine learning infrastructure using cloud computing platforms. Conducted data exploratory analysis to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
+			current: false,
+		},
+		{
+			role: "Machine Learning Engineer, ABX Corp. (2020-2021)",
+			description:
+				"Designed and implemented advanced machine learning algorithms to improve predictive accuracy and performance. Built and maintained scalable machine learning infrastructure using cloud computing platforms. Conducted data exploratory analysis to identify potential use cases and opportunities. Participated in regular code reviews to maintain high quality standards.",
+			current: false,
+		},
+	],
+};
